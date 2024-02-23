5e886f3bf71e7e9a76b4d32ede77ac588c1e6126
raflifaissalmustofa@gmail.com <raflifaisalmustofa@gmail.com>
2024-02-23 14:57:16 +0700

    update

--- database/seeders/AddStatusSuratKeluarSeeder.php
+++ database/seeders/AddStatusSuratKeluarSeeder.php
@@ -17,57 +17,68 @@ class AddStatusSuratKeluarSeeder extends Seeder
             [
                 'name' => 'Dibuat',
                 'description' => 'Surat telah dibuat',
-                'tipe_surat' => 'keluar'
+                'tipe_surat' => 'keluar',
+                'kode_status' => '201'
             ],
             [
                 'name' => 'Diminta penomoran surat',
                 'description' => 'Surat telah diminta nomor surat',
-                'tipe_surat' => 'keluar'
+                'tipe_surat' => 'keluar',
+                'kode_status' => '202'
             ],
             [
                 'name' => 'Diterima',
                 'description' => 'Surat telah diterima',
-                'tipe_surat' => 'keluar'
+                'tipe_surat' => 'keluar',
+                'kode_status' => '203'
             ],
             [
                 'name' => 'Diteruskan',
                 'description' => 'Surat telah diteruskan',
-                'tipe_surat' => 'keluar'
+                'tipe_surat' => 'keluar',
+                'kode_status' => '204'
             ],
             [
                 'name' => 'Diberi tanda tangan',
                 'description' => 'Surat telah diberi tanda tangan',
-                'tipe_surat' => 'keluar'
+                'tipe_surat' => 'keluar',
+                'kode_status' => '205'
             ],
             [
                 'name' => 'Diberi nomor dan diagendakan',
                 'description' => 'Surat telah diberi nomor dan diagendakan',
-                'tipe_surat' => 'keluar'
+                'tipe_surat' => 'keluar',
+                'kode_status' => '206'
             ],
             [
                 'name' => 'Dikirim ke tujuan',
                 'description' => 'Surat telah dikirim ke tujuan',
-                'tipe_surat' => 'keluar'
+                'tipe_surat' => 'keluar',
+                'kode_status' => '207'
             ],
             [
                 'name' => 'Diterima di tujuan',
                 'description' => 'Surat telah diterima di tujuan',
-                'tipe_surat' => 'keluar'
+                'tipe_surat' => 'keluar',
+                'kode_status' => '208'
             ],
             [
                 'name' => 'Dikembalikan',
                 'description' => 'Surat telah dikembalikan dengan catatan',
-                'tipe_surat' => 'keluar'
+                'tipe_surat' => 'keluar',
+                'kode_status' => '209'
             ],
             [
                 'name' => 'Telah disesuaikan',
                 'description' => 'Surat telah dikirimkan ke tujuan',
-                'tipe_surat' => 'keluar'
+                'tipe_surat' => 'keluar',
+                'kode_status' => '210'
             ],
             [
                 'name' => 'Diminta penomoran surat kembali',
                 'description' => 'Surat telah diminta penomoran surat kembali',
-                'tipe_surat' => 'keluar'
+                'tipe_surat' => 'keluar',
+                'kode_status' => '211'
             ],
         );
 
